9fc6654f204de4b943287300c2e85ae6c2251eff
radu <radu.helstern@deskpro.com>
2017-04-19 15:10:25 +0100

    dropped build hashes from filename fix distribution build

--- src/webpack/webpack.config-dist.js
+++ src/webpack/webpack.config-dist.js
@@ -6,16 +6,19 @@ const ManifestPlugin = require('webpack-manifest-plugin');
 const ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
 const WebpackChunkHash = require('webpack-chunk-hash');
 
+const BuildUtils = require('./BuildUtils');
+
 const PROJECT_ROOT_PATH = path.resolve(__dirname, '../../');
 const PRODUCTION = !process.env.NODE_ENV || process.env.NODE_ENV === 'production';
 const SLIM_PACKAGE = process.env.DPA_PACKAGE_MODE === 'slim';
 
-const PACKAGE_NAMES='deskpro+apps react';
-const artifactName = (baseName) => PACKAGE_NAMES.replace(/\+/, '').split(' ').concat([baseName]).join('-');
-
 const configParts = [];
 configParts.push({
     devtool: 'source-map',
+    externals: {
+      'react': 'React',
+      'react-dom': 'ReactDOM'
+    },
     module: {
         loaders: [
             {
@@ -28,8 +31,9 @@ configParts.push({
             { test: /\.(html?|css)$/, loader: 'raw-loader' }
         ],
         noParse: [
-            path.resolve(PROJECT_ROOT_PATH, 'node_modules/xcomponent/dist/xcomponent.js'),
-            path.resolve(PROJECT_ROOT_PATH, 'node_modules/post-robot/dist/post-robot.js')
+            path.resolve(PROJECT_ROOT_PATH, 'node_modules/deskproapps-sdk-core/dist'),
+            path.resolve(PROJECT_ROOT_PATH, 'node_modules/handlebars'),
+            path.resolve(PROJECT_ROOT_PATH, 'semantic-ui-react/dist')
         ]
     },
     plugins: [],
@@ -38,9 +42,13 @@ configParts.push({
             'meteor/check': 'empty-module', // not used, but required by uniforms
             'meteor/aldeed:simple-schema': 'empty-module', // not used, but required by uniforms
             'graphql': 'empty-module', // not used, but required by uniforms
-            handlebars: 'handlebars/dist/handlebars.js', // pulled in by simpl-schema, https://github.com/wycats/handlebars.js/issues/953
-            'deskproapps-sdk-core': 'deskproapps-sdk-core/dist/deskproapps-core-c150404c5f653b09f110',
+            //handlebars (pulled in by simpl-schema, https://github.com/wycats/handlebars.js/issues/953), resolved in babelrc
+            //'handlebars': 'handlebars/runtime.js',
+            'handlebars': 'handlebars/dist/handlebars.js',
+            'semantic-ui-react/src': 'semantic-ui-react/dist/commonjs',
+            'deskproapps-sdk-core': 'deskproapps-sdk-core/dist/deskproapps-sdk-core'
         },
+
         extensions: ['*', '.js', '.jsx', '.scss', '.css']
     },
     // stats: 'minimal',
@@ -52,12 +60,12 @@ if (SLIM_PACKAGE) {
     configParts.push({
         entry: {
             slim: [ path.resolve(PROJECT_ROOT_PATH, 'src/main/javascript/index.js') ],
-            vendor: [ 'lodash', 'react', 'react-dom', 'semantic-ui-react', 'simpl-schema', 'uniforms', 'deskproapps-sdk-core' ],
+            vendor: [ 'lodash', 'semantic-ui-react', 'simpl-schema', 'uniforms', 'deskproapps-sdk-core' ],
         },
         output: {
             pathinfo: !PRODUCTION,
-            chunkFilename: artifactName('[name].[hash].js'),
-            filename: artifactName('[name].[hash].js'),
+            chunkFilename: BuildUtils.artifactName('[name].js'),
+            filename: BuildUtils.artifactName('[name].js'),
             path: path.resolve(PROJECT_ROOT_PATH, 'dist')
         },
         plugins: [
@@ -70,10 +78,10 @@ if (SLIM_PACKAGE) {
             // vendor libs + extracted manifest
             new webpack.optimize.CommonsChunkPlugin({ name: ['vendor', 'manifest'], minChunks: Infinity }),
             // export map of chunks that will be loaded by the extracted manifest
-            new ChunkManifestPlugin({ filename: artifactName('manifest.json'), manifestVariable: 'DeskproAppsReactManifest' }),
+            new ChunkManifestPlugin({ filename: BuildUtils.artifactName('manifest.json'), manifestVariable: 'DeskproAppsSDKReactManifest' }),
 
             // mapping of all source file names to their corresponding output file
-            new ManifestPlugin({ fileName: artifactName('asset-manifest.json') }),
+            new ManifestPlugin({ fileName: BuildUtils.artifactName('asset-manifest.json') }),
         ],
     });
 } else {
@@ -82,16 +90,16 @@ if (SLIM_PACKAGE) {
         output: {
             libraryTarget: 'umd',
             umdNamedDefine: true,
-            library: 'DeskproAppsReact',
+            library: 'DeskproAppsSDKReact',
 
             pathinfo: !PRODUCTION,
-            chunkFilename: artifactName('[hash].js'),
-            filename: artifactName('[hash].js'),
+            chunkFilename: BuildUtils.artifactName('.js'),
+            filename: BuildUtils.artifactName('.js'),
             path: path.resolve(PROJECT_ROOT_PATH, 'dist')
         },
         plugins: [
             new webpack.DefinePlugin({ PRODUCTION: PRODUCTION }),
-            // for stable builds, in production we replace the default module index with the module's content hashe
+            // for stable builds, in production we replace the default module index with the module's content hash
             (PRODUCTION ? new webpack.HashedModuleIdsPlugin() : new webpack.NamedModulesPlugin()),
         ],
     });
