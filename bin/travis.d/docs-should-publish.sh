333ba284c49fe8e9ca477ad1f4db3b287b4087e3
radu <radu.helstern@deskpro.com>
2017-08-09 17:26:31 +0100

    add make-docs command and fix travis docs publishing script

--- bin/travis.d/docs-should-publish.sh
+++ bin/travis.d/docs-should-publish.sh
@@ -2,7 +2,7 @@
 
 SOURCE_BRANCH="master"
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "{$TRAVIS_PULL_REQUEST}" != "false" -o "${TRAVIS_BRANCH}" != "${SOURCE_BRANCH}" ]; then
+if [ "{$TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "${SOURCE_BRANCH}" ]; then
     exit 0
 else
     exit 1
